44bd23179a1c0307ead1659782c3198aa5ac9e90
Jeandra <jeandra8789@gmail.com>
2023-01-04 22:23:15 -0300

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,42 @@
-console.log("Teste teste teste")
+const apiKey = "";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelector("#suggestions button");
+
+// Loader
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+};
+
+const getWeatherData = async (city) => {
+    toggleLoader();
+
+    const apiWeatherURL = 
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    toggleLoader();
+
+    return data;
+};
+
+
